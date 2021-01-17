c95ef27e4795dc7b085a7ca9c3ee1fdb59430da1
Sway <56074112+swxy@users.noreply.github.com>
2021-01-16 18:40:36 -0800

    car shit extra

---
+++ resources/WiseGuy-Wheels/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+  'jdmrimscontentunlocks.meta',
+}
+
+data_file 'JDM_RIMS_CONTENT_UNLOCKS' 'jdmrimscontentunlocks.meta'
+
+client_script {
+    'vehicle_names.lua'
+}
---
+++ resources/[mod]/yuzler/__resource.lua
@@ -0,0 +1,3 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
